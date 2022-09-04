44a97526b47ad5a011e996a7b0bba60ebb91c4a2
Lonedeadly <Lonedeadly@yandex.ru>
2022-09-04 15:26:22 +0300

    release program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
 ﻿
+Console.Write("Введите количество строк: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string[] FirstArray = new string[N];
+string[] SecondArray = new string[N];
+Console.WriteLine();
+
+int p = 0;
+for (int i = 0; i < N; i++)
+{
+    Console.Write("Введите текст в строку " + (i+1) + ": ");
+    string ? temp = Console.ReadLine();
+    if (temp == null) temp = "";
+    FirstArray[i] = temp;
+    if (temp.Length <= 3)
+    {
+        SecondArray[p] = temp;
+        p++;
+    }       
+}
+
+Console.WriteLine();
+Console.WriteLine("Изначальный Массив: [" + String.Join(", ", FirstArray) + "]");
+Console.WriteLine("Результат: [" + String.Join(", ", SecondArray, 0, p) + "]");
